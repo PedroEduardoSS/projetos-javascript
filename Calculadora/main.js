01974120db4937944965f0c93235a25733e485e1
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-04-29 11:08:57 -0300

    Melhorias

---
+++ Calculadora/main.js
@@ -0,0 +1,42 @@
+// Tela
+const tela = document.getElementById("tela");
+tela.innerHTML = " " + " " + " ";
+
+// Número
+let zero = document.getElementById("zero");
+//zero.addEventListener("click", addNum);
+let um = document.getElementById("um");
+um.addEventListener("click", addUm);
+let dois = document.getElementById("dois");
+let tres = document.getElementById("tres");
+let quatro = document.getElementById("quatro");
+let cinco = document.getElementById("cinco");
+let seis = document.getElementById("seis");
+let sete = document.getElementById("sete");
+let oito = document.getElementById("oito");
+let nove = document.getElementById("nove");
+
+const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// Função para adicionar número
+function addUm() {
+    
+}
+
+// Função para adicionar operação
+function addOper() {
+    let soma = document.getElementById("soma");
+    let sub = document.getElementById("sub");
+    let multi = document.getElementById("multi");
+    let divi = document.getElementById("divi");
+}
+
+// Função para limpar a tela
+function limpar() {
+    let limpar = document.getElementById("limpar");
+}
+
+// Função para efetuar a operação
+function main() {
+    let main = document.getElementById("igual");
+}
