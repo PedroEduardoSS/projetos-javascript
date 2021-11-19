1824e272a6d904aeebf1e654244faf514cb11146
PedroEduardoSS <pedroeduardossousa@gmail.com>
2021-11-19 10:39:31 -0300

    transferred projects to projetos-node repository

--- Node-FileHandling/stream.js
+++
@@ -1,7 +0,0 @@
-const fs = rrequire('fs')
-const readStream = fs.createReadStream('./test.txt', 'utf-8')
-const writeStream = fs.createReadStream()
-
-readStream.on('data', (chunk)=>{
-    writeStream.write(chunk);
-})
